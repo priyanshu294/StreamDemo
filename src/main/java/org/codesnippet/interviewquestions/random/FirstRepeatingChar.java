02f6ec528e9e385bcafd20eced2d6f598678f449
Priyanshu <ps875761@gmail.com>
2025-11-24 13:11:58 +0530

    Added code in random

---
+++ src/main/java/org/codesnippet/interviewquestions/random/FirstRepeatingChar.java
@@ -0,0 +1,31 @@
+package org.codesnippet.interviewquestions.random;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FirstRepeatingChar {
+    public static void main(String[] args) {
+
+        String str = "abca";
+
+        Character firstRepeating = str.chars()
+                .mapToObj(c -> (char)c)
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        LinkedHashMap::new,
+                        Collectors.counting()
+                ))
+
+                .entrySet()
+                .stream()
+                .filter(e->e.getValue()>1)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(null);
+
+        System.out.println(firstRepeating);
+
+    }
+}
